d5bc425257ced6da6bd41d4d0a0370495494028e
alon104 <rosenfeld@mail.tau.ac.il>
2022-03-28 13:34:26 +0300

    small changes

--- avl_skeleton.py
+++ avl_skeleton.py
@@ -12,9 +12,9 @@ class AVLNode(object):
     """Constructor, you are allowed to add more fields.
 
 
-	@type value: str
-	@param value: data of your node
-	"""
+    @type value: str
+    @param value: data of your node
+    """
 
     def __init__(self, value):
         self.value = value
@@ -25,9 +25,9 @@ class AVLNode(object):
         self.size = 0  # this supposed to be maintain in the tree class
 
     """returns the left child
-	@rtype: AVLNode
-	@returns: the left child of self, None if there is no left child
-	"""
+    @rtype: AVLNode
+    @returns: the left child of self, None if there is no left child
+    """
 
     def getLeft(self):
         if self.left.isRealNode():
@@ -36,9 +36,9 @@ class AVLNode(object):
 
     """returns the right child
 
-	@rtype: AVLNode
-	@returns: the right child of self, None if there is no right child
-	"""
+    @rtype: AVLNode
+    @returns: the right child of self, None if there is no right child
+    """
 
     def getRight(self):
         if self.right.isRealNode():
@@ -47,9 +47,9 @@ class AVLNode(object):
 
     """returns the parent 
 
-	@rtype: AVLNode
-	@returns: the parent of self, None if there is no parent
-	"""
+    @rtype: AVLNode
+    @returns: the parent of self, None if there is no parent
+    """
 
     def getParent(self):
         if self.parent.isRealNode():
@@ -58,9 +58,9 @@ class AVLNode(object):
 
     """return the value
 
-	@rtype: str
-	@returns: the value of self, None if the node is virtual
-	"""
+    @rtype: str
+    @returns: the value of self, None if the node is virtual
+    """
 
     def getValue(self):
         if self.isRealNode():
@@ -70,9 +70,9 @@ class AVLNode(object):
 
     """returns the height
 
-	@rtype: int
-	@returns: the height of self, -1 if the node is virtual
-	"""
+    @rtype: int
+    @returns: the height of self, -1 if the node is virtual
+    """
 
     def getHeight(self):
         if self.isRealNode():
@@ -81,9 +81,9 @@ class AVLNode(object):
 
     """sets left child
 
-	@type node: AVLNode
-	@param node: a node
-	"""
+    @type node: AVLNode
+    @param node: a node
+    """
 
     def setLeft(self, node):
         if self.left.isRealNode():
@@ -92,9 +92,9 @@ class AVLNode(object):
 
     """sets right child
 
-	@type node: AVLNode
-	@param node: a node
-	"""
+    @type node: AVLNode
+    @param node: a node
+    """
 
     def setRight(self, node):
         if self.right.isRealNode():
@@ -103,9 +103,9 @@ class AVLNode(object):
 
     """sets parent
 
-	@type node: AVLNode
-	@param node: a node
-	"""
+    @type node: AVLNode
+    @param node: a node
+    """
 
     def setParent(self, node):
         if self.parent.isRealNode():
@@ -114,9 +114,9 @@ class AVLNode(object):
 
     """sets value
 
-	@type value: str
-	@param value: data
-	"""
+    @type value: str
+    @param value: data
+    """
 
     def setValue(self, value):
         if self.isRealNode():
@@ -125,18 +125,18 @@ class AVLNode(object):
 
     """sets the balance factor of the node
 
-	@type h: int
-	@param h: the height
-	"""
+    @type h: int
+    @param h: the height
+    """
 
     def setHeight(self, h):
         self.height = h
 
     """returns whether self is not a virtual node 
 
-	@rtype: bool
-	@returns: False if self is a virtual node, True otherwise.
-	"""
+    @rtype: bool
+    @returns: False if self is a virtual node, True otherwise.
+    """
 
     def isRealNode(self):
         if self.height == -1:
@@ -180,9 +180,9 @@ A class implementing the ADT list, using an AVL tree.
 
 class AVLTreeList(object):
     """
-	Constructor, you are allowed to add more fields.
+    Constructor, you are allowed to add more fields.
 
-	"""
+    """
 
     def __init__(self):
         self.root = None
@@ -192,9 +192,9 @@ class AVLTreeList(object):
 
     """returns whether the list is empty
 
-	@rtype: bool
-	@returns: True if the list is empty, False otherwise
-	"""
+    @rtype: bool
+    @returns: True if the list is empty, False otherwise
+    """
 
     def empty(self):
         if self.root.height == -1:
@@ -204,146 +204,146 @@ class AVLTreeList(object):
 
     """retrieves the value of the i'th item in the list
 
-	@type i: int
-	@pre: 0 <= i < self.length()
-	@param i: index in the list
-	@rtype: str
-	@returns: the the value of the i'th item in the list
-	"""
+    @type i: int
+    @pre: 0 <= i < self.length()
+    @param i: index in the list
+    @rtype: str
+    @returns: the the value of the i'th item in the list
+    """
 
-	def retrieve(self, i):
-		if i < 0 or !self.root.isRealNode() or self.root.rank > i+1:
-			return None
-		return retrieve_rec(self.root, i+1)
+    def retrieve(self, i):
+        if i < 0 or not self.root.isRealNode() or self.root.size > i+1:
+            return None
+        return retrieve_rec(self.root, i+1)
 
 
     """inserts val at position i in the list
 
-	@type i: int
-	@pre: 0 <= i <= self.length()
-	@param i: The intended index in the list to which we insert val
-	@type val: str
-	@param val: the value we inserts
-	@rtype: list
-	@returns: the number of rebalancing operation due to AVL rebalancing
-	"""
+    @type i: int
+    @pre: 0 <= i <= self.length()
+    @param i: The intended index in the list to which we insert val
+    @type val: str
+    @param val: the value we inserts
+    @rtype: list
+    @returns: the number of rebalancing operation due to AVL rebalancing
+    """
 
     def insert(self, i, val):
         return -1
 
     """deletes the i'th item in the list
 
-	@type i: int
-	@pre: 0 <= i < self.length()
-	@param i: The intended index in the list to be deleted
-	@rtype: int
-	@returns: the number of rebalancing operation due to AVL rebalancing
-	"""
+    @type i: int
+    @pre: 0 <= i < self.length()
+    @param i: The intended index in the list to be deleted
+    @rtype: int
+    @returns: the number of rebalancing operation due to AVL rebalancing
+    """
 
     def delete(self, i):
         return -1
 
     """returns the value of the first item in the list
 
-	@rtype: str
-	@returns: the value of the first item, None if the list is empty
-	"""
+    @rtype: str
+    @returns: the value of the first item, None if the list is empty
+    """
     
-	def first(self):
-		return self.first
+    def first(self):
+        return self.first
 
     """returns the value of the last item in the list
 
-	@rtype: str
-	@returns: the value of the last item, None if the list is empty
-	"""
-    
-	def last(self):
-		return self.last
+    @rtype: str
+    @returns: the value of the last item, None if the list is empty
+    """
+
+    def last(self):
+        return self.last
 
 
 
     """returns an array representing list 
 
-	@rtype: list
-	@returns: a list of strings representing the data structure
-	"""
+    @rtype: list
+    @returns: a list of strings representing the data structure
+    """
 
     def listToArray(self):
         return None
 
     """returns the size of the list 
 
-	@rtype: int
-	@returns: the size of the list
-	"""
+    @rtype: int
+    @returns: the size of the list
+    """
 
-	def length(self):
-		return self.length
+    def length(self):
+        return self.length
 
 
     """splits the list at the i'th index
 
-	@type i: int
-	@pre: 0 <= i < self.length()
-	@param i: The intended index in the list according to whom we split
-	@rtype: list
-	@returns: a list [left, val, right], where left is an AVLTreeList representing the list until index i-1,
-	right is an AVLTreeList representing the list from index i+1, and val is the value at the i'th index.
-	"""
+    @type i: int
+    @pre: 0 <= i < self.length()
+    @param i: The intended index in the list according to whom we split
+    @rtype: list
+    @returns: a list [left, val, right], where left is an AVLTreeList representing the list until index i-1,
+    right is an AVLTreeList representing the list from index i+1, and val is the value at the i'th index.
+    """
 
     def split(self, i):
         return None
 
     """concatenates lst to self
 
-	@type lst: AVLTreeList
-	@param lst: a list to be concatenated after self
-	@rtype: int
-	@returns: the absolute value of the difference between the height of the AVL trees joined
-	"""
+    @type lst: AVLTreeList
+    @param lst: a list to be concatenated after self
+    @rtype: int
+    @returns: the absolute value of the difference between the height of the AVL trees joined
+    """
 
     def concat(self, lst):
         return None
 
     """searches for a *value* in the list
 
-	@type val: str
-	@param val: a value to be searched
-	@rtype: int
-	@returns: the first index that contains val, -1 if not found.
-	"""
+    @type val: str
+    @param val: a value to be searched
+    @rtype: int
+    @returns: the first index that contains val, -1 if not found.
+    """
 
-	def search(self, val):
-		avl_list = self.listToArray()
-		for i in range(self.length):
-			if avl_list[i] == val:
-				return i
-		return -1
+    def search(self, val):
+        avl_list = self.listToArray()
+        for i in range(self.length):
+            if avl_list[i] == val:
+                return i
+        return -1
 
 
 
     """returns the root of the tree representing the list
 
-	@rtype: AVLNode
-	@returns: the root, None if the list is empty
-	"""
+    @rtype: AVLNode
+    @returns: the root, None if the list is empty
+    """
 
     def getRoot(self):
         return None
 
 
 def retrieve_rec(root, i):
-	if root.left.isRealNode():
-		left_sub = root.left.rank + 1
-		if left_sub == i:
-			return left_sub
-		elif i < left_sub:
-			return retrieve_rec(root.left, i)
-		else:
-			return retrieve_rec(root.right, i - left_sub)
-	elif i == 1: ##if no sub_tree to the left
-		return root
-	elif root.right.isRealNode():
-		return retrieve_rec(root.right, i - 1)
+    if root.left.isRealNode():
+        left_sub = root.left.rank + 1
+        if left_sub == i:
+            return left_sub
+        elif i < left_sub:
+            return retrieve_rec(root.left, i)
+        else:
+            return retrieve_rec(root.right, i - left_sub)
+    elif i == 1: ##if no sub_tree to the left
+        return root
+    elif root.right.isRealNode():
+        return retrieve_rec(root.right, i - 1)
 
