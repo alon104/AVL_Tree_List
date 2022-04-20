4c03700b3e87e34b2c3bffc6c93875cd265ecd87
alon104 <rosenfeld@mail.tau.ac.il>
2022-04-20 19:43:45 +0300

    final version

--- AVLTreeList.py
+++ AVLTreeList.py
@@ -4,8 +4,6 @@
 # id2      - 313247900
 # name2    - omri ravona
 
-from utils.print_tree import printree
-
 """A class represnting a node in an AVL tree"""
 
 class AVLNode(object):
@@ -940,7 +938,6 @@ class AVLTreeList(object):
     def getRoot(self):
         return self.root
 
-##functions for guy's tester
     def append(self, val):
         return self.insert(self.length(), val)
 
